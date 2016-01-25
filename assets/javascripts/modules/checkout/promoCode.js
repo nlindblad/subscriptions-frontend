fc0aa6a2ef8cda528caaa4b6fca7ed6a2edc823f
Paul Brown <paul.brown@guardian.co.uk>
2016-01-25 09:01:25 +0000

    Code style tweaks.

--- assets/javascripts/modules/checkout/promoCode.js
+++ assets/javascripts/modules/checkout/promoCode.js
@@ -10,31 +10,37 @@ define([
 
     var getValidity = function () {
 
-        var promoCode = formElements.$PROMO_CODE[0].value.trim();
+        var country    = countryChoice.getCurrentCountryOption().val(),
+            $inputBox  = formElements.$PROMO_CODE,
+            lookupUrl  = $inputBox.data('lookup-url'),
+            promoCode  = $inputBox.val().trim(),
+            ratePlanId = formElements.getRatePlanId();
+
         if (promoCode == '') {
             return;
         }
-        console.log(promoCode);
+
         ajax({
+            type: 'json',
             method: 'GET',
-            url: '/checkout/lookupPromotion', //fixme: get this endpoint actually *from* somewhere
+            url: lookupUrl,
             data: {
                 promoCode: promoCode,
-                productRatePlanId: formElements.getRatePlanId(),
-                country: countryChoice.getCurrentCountryOption().value
+                productRatePlanId: ratePlanId,
+                country: country
             }
         }).then(function (a) {
-
-            toggleError(formElements.$PROMO_CODE, false);
-
+            toggleError($inputBox, false);
             $('.js-promo-code-message').text(a.promotion.description);
-            $('.js-promo-code-image').src=a.thumbnailUrl;
+            $('.js-promo-code-image').attr('src', a.thumbnailUrl);
         }).catch(function (a) {
             //I think that I've made some mistake with the html which is causing this toggleError to never really set an error
             toggleError(formElements.$PROMO_CODE, true);
+            // Content of error codes are not parsed by ajax/reqwest.
             var b = JSON.parse(a.response);
             console.log(b);
-
+            $('.js-promo-code-message').text(b.message);
+            $('.js-promo-code-image').attr('src', a.thumbnailUrl);
         })
     };
 
